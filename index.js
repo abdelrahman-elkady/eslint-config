62d2ed02099834f43bf582b51b9ce76400ee0dda
kady <elkadyabdelrahman@gmail.com>
2018-04-11 07:19:00 +0200

    Incremental additions :see_no_evil:

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 module.exports = {
   extends: 'eslint:recommended',
-
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: 'module',
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true
+    }
+  },
+  env: {
+    node: true,
+    browser: true,
+    commonjs: true,
+    jest: true
+  },
   rules: {
-
+    indent: ['error', 2, { SwitchCase: 1 }],
+    quotes: ['error', 'single'],
+    'no-unused-vars': ['error', { args: 'none' }],
+    'no-multiple-empty-lines': ['error', {max: 1}],
+    'eol-last': ['error', 'always']
   }
 };
