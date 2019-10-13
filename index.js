7f3b9fdea877c7d286e40827f3ed22e2f7eea102
Abdelrahman Elkady <elkadyabdelrahman@gmail.com>
2019-10-13 21:47:01 +0200

    Extend max-len to 120 character per line

--- index.js
+++ index.js
@@ -10,5 +10,13 @@ module.exports = {
     'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
 
     'no-unused-vars': ['error', { vars: 'all', args: 'none', ignoreRestSiblings: true }],
+
+    'max-len': ['error', 120, 2, {
+      ignoreUrls: true,
+      ignoreComments: false,
+      ignoreRegExpLiterals: true,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true,
+    }],
   }
 };
